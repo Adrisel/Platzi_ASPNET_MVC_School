1919632d90d14b27a38eddf6a329efde75fe834b
adrisel <elecadrinf@gmail.com>
2021-02-10 22:37:08 -0400

    seed the students lista and render it into a table

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SchoolMVC.Models;
 
@@ -17,15 +18,19 @@ namespace SchoolMVC.Controllers
 
         public IActionResult MultipleStudent()
         {
-             List<Student> studentList = new List<Student>()
-                {
-                    new Student(){Name = "Carla"},
-                    new Student(){Name = "Rudy"},
-                    new Student(){Name = "Rafael"},
-                    new Student(){Name = "Isabel"},
-
-                };
+             List<Student> studentList = GenerateStudents(30);
             return View(studentList);
+        }
+
+         private List<Student> GenerateStudents(int quantity)
+        {
+            string[] name1 = new string[] { "Adriana", "Marcia", "Ronald", "Mauricio", "Daniel" };
+            string[] name2 = new string[] { "Tito", "Camacho", "Sanchez", "Salazar", "Orellana" };
+
+            var studentsList = from n1 in name1
+                               from n2 in name2
+                               select new Student() { Name = $"{n1} {n2}" };
+            return studentsList.OrderBy(std => std.Id).Take(quantity).ToList();
         }
     }
 }
