67621f6144778f0163fe5feb1d75fffac4824285
adrisel <elecadrinf@gmail.com>
2021-02-17 21:59:24 -0400

    inject the context and specify the database engine

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SchoolMVC.Models;
 
 namespace SchoolMVC
 {
@@ -24,6 +26,10 @@ namespace SchoolMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SchoolContext>(
+                options => options.UseInMemoryDatabase(databaseName: "testDB")
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
