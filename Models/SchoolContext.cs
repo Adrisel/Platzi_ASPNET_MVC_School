916d3f01fdb79a040aeb5df019afbb45dd7dbf15
adrisel <elecadrinf@gmail.com>
2021-02-21 18:12:56 -0400

    refactor de seeder with the relation between models

--- Models/SchoolContext.cs
+++ Models/SchoolContext.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -13,7 +14,7 @@ namespace SchoolMVC.Models
 
         public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
         {
-            
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -28,29 +29,72 @@ namespace SchoolMVC.Models
                 SchoolType = SchoolType.Elementary,
                 YearFoundation = 1970
             };
+
+            //Cargar cursos de la escuela
+            var courses = AddCourses(school);
+            //x cada curso cargar asignaturas
+            var subjects = AddSubject(courses); 
+            // x cada curso cargar alumnos
+            var students = AddStudents(courses);
+
             modelBuilder.Entity<School>().HasData(school);
-
-            List<Subject> subjectList = new List<Subject>()
-            {
-                new Subject(){Name = "Maths"},
-                new Subject(){Name = "Spanish"},
-                new Subject(){Name = "Science"},
-                new Subject(){Name = "Sports"},
-            };
-            modelBuilder.Entity<Subject>().HasData(subjectList.ToArray());
-
-            modelBuilder.Entity<Student>().HasData(GenerateStudents(20).ToArray());
+            modelBuilder.Entity<Course>().HasData(courses.ToArray());
+            modelBuilder.Entity<Subject>().HasData(subjects.ToArray());
+            modelBuilder.Entity<Student>().HasData(students.ToArray());
         }
 
-        private List<Student> GenerateStudents(int quantity)
+        private List<Course> AddCourses(School school)
+        {
+            return new List<Course>
+            {
+               new Course(){ Name = "101",TurnType = TurnType.Morning, SchoolId = school.Id},
+               new Course(){ Name = "201",TurnType = TurnType.Morning, SchoolId = school.Id},
+               new Course(){ Name = "301",TurnType = TurnType.Morning, SchoolId = school.Id},
+               new Course(){ Name = "401",TurnType = TurnType.Morning, SchoolId = school.Id},
+               new Course(){ Name = "501",TurnType = TurnType.Morning, SchoolId = school.Id}
+            };
+        }
+
+        private List<Subject> AddSubject(List<Course> courses)
+        {
+            List<Subject> completeList = new List<Subject>();
+            foreach (var course in courses)
+            {
+                var tempList = new List<Subject>()
+                {
+                    new Subject(){Name = "Maths", CourseId = course.Id},
+                    new Subject(){Name = "Spanish", CourseId = course.Id},
+                    new Subject(){Name = "Science", CourseId = course.Id},
+                    new Subject(){Name = "Sports", CourseId = course.Id},
+                };
+
+                completeList.AddRange(tempList);
+            }
+            return completeList;
+        }
+
+        private List<Student> GenerateStudents(int quantity, Course course)
         {
             string[] name1 = new string[] { "Adriana", "Marcia", "Ronald", "Mauricio", "Daniel" };
             string[] name2 = new string[] { "Tito", "Camacho", "Sanchez", "Salazar", "Orellana" };
 
             var studentsList = from n1 in name1
                                from n2 in name2
-                               select new Student() { Name = $"{n1} {n2}" };
+                               select new Student() { Name = $"{n1} {n2}" , CourseId = course.Id};
             return studentsList.OrderBy(std => std.Id).Take(quantity).ToList();
+        }
+
+         private List<Student> AddStudents(List<Course> courses)
+        {
+            Random random = new Random();
+            var studentList = new List<Student>();
+            foreach (var course in courses)
+            {
+                int numeberOfStudent = random.Next(5, 20);
+                var tempList = GenerateStudents(numeberOfStudent, course);
+                studentList.AddRange(tempList);
+            }
+            return studentList;
         }
     }
 }
